2215bbd8faadc13c2bfd804e9831eebd10b06c23
abrish2082 <61533602+abrish2082@users.noreply.github.com>
2021-07-16 05:08:54 -0800

    connected when pushed
    ok

--- simple.java
+++ simple.java
@@ -2,10 +2,6 @@ class Simple{
     public static void main(String args[]){  
       for(int i=1; i<=10;i++){
  System.out.println("Hello jenkins and java  .... anad git..... " +i) ; 
-
- 
-   
-
   }    
     }  
 }   
