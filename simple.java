b2475b3e85857d94c173bc8f7cc7ef71ddb10ef7
abrish2082 <61533602+abrish2082@users.noreply.github.com>
2021-08-25 07:05:32 -0700

    commeneted
    comment

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-      for(int i=1; i<=5;i++){
+      for(int i=1; i<=4;i++){ 
  System.out.println("Hello jenkins and java  .... anad git..... " +i) ; 
   }    
     }    
