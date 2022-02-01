b6e0a2d196ec385e41c31ba08c14c6c36980ed07
abrish2082 <61533602+abrish2082@users.noreply.github.com>
2022-02-01 01:02:02 -0800

    soo ssssoo

--- simple.java
+++ simple.java
@@ -2,7 +2,7 @@ class Simple{
     public static void main(String args[]){  
       for(int i=1; i<=4;i++){
  
- System.out.println("Hello jenkins and ... git.....  I am simple java code " +i) ; 
+ System.out.println("Hello jenkins and ... git.....  I am simple java code so ..... " +i ) ; 
   }    
     }      
           
