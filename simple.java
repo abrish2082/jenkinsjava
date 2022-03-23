76ce09e67889d499f52aa3bd0a25f7811ca61246
abrish2082 <61533602+abrish2082@users.noreply.github.com>
2022-03-23 08:57:47 -0300

    updated
    gffgf

--- simple.java
+++ simple.java
@@ -1,10 +1,10 @@
 class Simple{  
     public static void main(String args[]){  
-      for(int i=1; i<=5;i++){
+      for(int i=1; i<=4;i++){
  
  System.out.println("Hello jenkins and ... git.....  I am simple java code so ..... " +i ) ; 
   }      
-    }    
+    }
                              
 }        
  
