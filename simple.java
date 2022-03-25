e6a79a7fc47a432ee648e58d083229b0acca91ad
abrish2082 <61533602+abrish2082@users.noreply.github.com>
2022-03-25 08:57:23 -0300

    cchanged

--- simple.java
+++ simple.java
@@ -3,24 +3,9 @@ class Simple{
       for(int i=1; i<=4;i++){
  
  System.out.println("Hello jenkins and ... git.....  I am simple java code so ..... " +i ) ; 
-<<<<<<< HEAD
   }      
     }
                          
 }
 
 
- 
-=======
-  }         
-    }  
-                                  
-}   
-<<<<<<< HEAD
->>>>>>> 7ddf8f4cf36f72f971ca90da7e7694917307af40
-  
-       
-=======
-      
-         
->>>>>>> fad8d1dfd342afd40ee122e7e3629da482a3d359
