03df7999fde0ba150a0da69439e5b0a44543223e
abrish2082 <61533602+abrish2082@users.noreply.github.com>
2022-03-25 09:23:09 -0300

    pushed

--- simple.java
+++ simple.java
@@ -3,7 +3,7 @@ class Simple{
       for(int i=1; i<=4;i++){
  
  System.out.println("Hello jenkins and ... git.....  I am simple java code so ..... " +i ) ; 
-  }      
+  }        
     }  
                            
 }     
