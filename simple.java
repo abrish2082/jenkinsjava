f4db917471265f79189732888398ae336dcd8448
abreham <abrish2082@gmail.com>
2022-03-25 09:29:40 -0300

    done pushed

--- simple.java
+++ simple.java
@@ -3,12 +3,12 @@ class Simple{
       for(int i=1; i<=4;i++){
  
  System.out.println("Hello jenkins and ... git.....  I am simple java code so ..... " +i ) ; 
-<<<<<<< HEAD
- System.out.println("Hello jenkins and ... git.....  I am simple java code so ..... " +i ) ;  
+
+
   }      
-=======
-  }        
->>>>>>> 03df7999fde0ba150a0da69439e5b0a44543223e
+
+        
+
     }  
                            
 }     
