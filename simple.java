03903ffe6513edd0f11e9f76a148a10285c24724
abrish2082 <61533602+abrish2082@users.noreply.github.com>
2022-09-13 11:10:01 +0300

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-      for(int i=1; i<=3;i++){
+      for(int i=1; i<=5;i++){
     
  System.out.println("Hello jenkins and ... git.....  I am simple java pushed automatically.... " +i ) ; 
 
